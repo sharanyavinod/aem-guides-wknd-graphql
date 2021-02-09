430d7e7e41ba9a053443b68d11291f1ef310ec71
Sharanya Vinod <sharanyavinod90@gmail.com>
2021-02-09 18:41:00 +0100

    Fixed typo

--- react-app/src/components/import-components.js
+++ react-app/src/components/import-components.js
@@ -1,6 +1,6 @@
 import { MapTo } from '@adobe/aem-react-editable-components';
 import { Text, TextEditConfig } from './AEMText';
-import Image, { ImageEditConfig } from './AEMText';
+import Image, { ImageEditConfig } from './AEMImage';
 
 MapTo('wknd-spa/components/text')(Text, TextEditConfig);
 MapTo('wknd-spa/components/image')(Image, ImageEditConfig);
